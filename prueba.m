aec02f65fff5158d7335703fb2154a023a1cfc0e
A01749606 <113162889+A01749606@users.noreply.github.com>
2022-11-17 05:04:57 +0000

    Prueba

--- prueba.m
+++ prueba.m
@@ -6,17 +6,16 @@ plot(x,y)
 
 % Datos
 del_t = input("Dame el tiempo: ");
-masa = input("Masa (kg)");
+masa = input("Masa (kg): ");
 v0 = input("Velocidad inicial: ");
-fric = input("Fricción: ");
-drag = input("Coeficiente de arrastre: ");
+% fric = input("Fricción: ");
+% drag = input("Coeficiente de arrastre: ");
 
 %% Animación
-%Random Data
 x = 0:50;
 y =(x.^3/500) - (3*x.^2)/35 + 3*cos((2*3.141592*x)/15) + 30;
 
-%Animated Figure
+%% Animated Figure
 figure;
 hold on;
 plot(x,y);
