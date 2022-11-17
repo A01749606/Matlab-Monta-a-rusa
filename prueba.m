ed734fe5c6930aff9b4273d8754f1174561dc5b5
A01749606 <113162889+A01749606@users.noreply.github.com>
2022-11-17 19:24:24 +0000

    Con Ek y Ep

--- prueba.m
+++ prueba.m
@@ -1,40 +1,53 @@
 clc, clear
+% Sacar velocidad negativa
+% Que se dejen de ver los puntos anteriores
 
 % Datos
-del_t = input("Dame el tamaño de paso del tiempo (s): ");
-v0 = input("Dame la velocidad inicial (m/s): ");
-x0 = 0;
 g = 9.81;
-y0 =(x0.^3/500) - (3*x0.^2)/35 + 3*cos((2*3.141592*x0)/15) + 30;
+
+del_t = input("Dame el tamaño de paso del tiempo (s): ");
 t = del_t;
 
+v0 = input("Dame la velocidad inicial (m/s): ");
+
+x0 = 27;
+y0 = (x0.^3/500) - (3*x0.^2)/35 + 3*cos((2*3.141592*x0)/15) + 30;
+yn = 0;
+
+
+
 % Vía
-x_via = 0:35;
+x_via = 0:40;
 y_via =(x_via.^3/500) - (3*x_via.^2)/35 + 3*cos((2*3.141592*x_via)/15) + 30;
 hold on;
 plot(x_via,y_via);
 
 
-% Punto que se mueve
-carro = plot(NaN, NaN, 'db');
+% Gráfica
 box on;
 grid on;
 
-% Gráfica
 while x0 <= 35
-
+    
+    
     % Velocidad y posición
-    % v = sqrt((v0.^2)-2*g(0-y0));
-    xn = x0 + v0 * t;
-    y =(xn.^3/500) - (3*xn.^2)/35 + 3*cos((2*3.141592*xn)/15) + 30;
+    v = sqrt( (v0.^2) - (2*g) * (yn-y0) );
+    xn = x0 + v * t;
+    yn = (xn.^3/500) - (3*xn.^2)/35 + 3*cos((2*3.141592*xn)/15) + 30;
+
+    % Actualizar valores de variables
+    v0 = v;
     x0 = xn;
+    y0 = yn;
     t = t + del_t;
 
-    fprintf("%f = %f , %f \n", t, xn, y)
+    fprintf("%f = %f , %f \n", t, xn, yn)
 
-    %set(carro, 'XData', xn, 'YData', y);
-    plot(xn,y,'db');
+    % Punto que se mueve
+    carro = plot(xn,yn,'db');
     drawnow
+    pause(t/10)
+    
     
 end
 hold off;
