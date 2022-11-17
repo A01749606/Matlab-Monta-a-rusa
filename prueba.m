d4a0095660a370c5b4365a81f7515b81c72f1a9d
A01749606 <113162889+A01749606@users.noreply.github.com>
2022-11-17 13:27:43 +0000

    No se borran puntos anteriores

--- prueba.m
+++ prueba.m
@@ -1,36 +1,46 @@
 clc, clear
-% Función de la montaña rusa
-x = 0:50;
-y =(x.^3/500) - (3*x.^2)/35 + 3*cos((2*3.141592*x)/15) + 30;
-plot(x,y)
 
 % Datos
-del_t = input("Dame el tiempo: ");
-masa = input("Masa (kg): ");
-v0 = input("Velocidad inicial: ");
-% fric = input("Fricción: ");
-% drag = input("Coeficiente de arrastre: ");
+del_t = input("Dame el tamaño de paso del tiempo (s): ");
+v0 = input("Dame la velocidad inicial (m/s): ");
+x0 = 0;
+g = 9.81;
+y0 =(x0.^3/500) - (3*x0.^2)/35 + 3*cos((2*3.141592*x0)/15) + 30;
+t = del_t;
 
-%% Animación
-x = 0:50;
-y =(x.^3/500) - (3*x.^2)/35 + 3*cos((2*3.141592*x)/15) + 30;
-
-%% Animated Figure
-figure;
+% Vía
+x_via = 0:35;
+y_via =(x_via.^3/500) - (3*x_via.^2)/35 + 3*cos((2*3.141592*x_via)/15) + 30;
 hold on;
-plot(x,y);
-plot(x(1),y(1),'db');
-axis([min(x) max(x) min(y) max(y)]);
-xlabel('X', 'Interpreter','Latex');
-ylabel('Y', 'Interpreter','Latex');
-grid on;
+plot(x_via,y_via);
+
+
+% Punto que se mueve
+carro = plot(NaN, NaN, 'db');
 box on;
-hPlot = plot(NaN, NaN, 'db');
-for k = 2:length(x) %loop
-    set(hPlot, 'XData', x(k), 'YData', y(k));
+grid on;
+
+% Gráfica
+while x0 <= 35
+
+    % Velocidad y posición
+    % v = sqrt((v0.^2)-2*g(0-y0));
+    xn = x0 + v0 * t;
+    y =(xn.^3/500) - (3*xn.^2)/35 + 3*cos((2*3.141592*xn)/15) + 30;
+    x0 = xn;
+    t = t + del_t;
+
+    fprintf("%f = %f , %f \n", t, xn, y)
+
+    %set(carro, 'XData', xn, 'YData', y);
+    plot(xn,y,'db');
     drawnow
+    
 end
 hold off;
+
+
+
 
 
 % X(i+1) = X(i)+V delta t
